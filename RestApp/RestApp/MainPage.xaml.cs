711269ff7d5814fbb37df53101b2675065a5dec5
Stella7778 <81565165+Stella7778@users.noreply.github.com>
2022-12-07 02:51:09 +0900

    [튜토리얼] 버튼생성

--- RestApp/RestApp/MainPage.xaml.cs
+++ RestApp/RestApp/MainPage.xaml.cs
@@ -14,5 +14,12 @@ namespace RestApp
         {
             InitializeComponent();
         }
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+
     }
 }
